1f9eab8391cb9d444df8ec871cb5f84e467c7a05
lorleod <taylor.mcleod@gmail.com>
2022-06-03 00:42:19 +0000

    blarg

--- 4_queries/9_cohort_avg_assist_dur.sql
+++ 4_queries/9_cohort_avg_assist_dur.sql
@@ -1,8 +1,9 @@
-SELECT avg(total_duration) as average_total_duration
+SELECT avg (total_duration) as average_total_duration
 FROM (
-  SELECT cohorts.name as cohort, sum(completed_at - started_at) as total_duration
-  FROM students
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  JOIN assistance_requests ON student_id = students.id
+  SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
   GROUP BY cohorts.name
-) as huh;
+  ORDER BY total_duration
+) as total_durations;
